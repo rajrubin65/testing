2e037d5764b4a5d155f7933b0fe58b8f428920dd
rajrubin65 <100127155+rajrubin65@users.noreply.github.com>
2023-10-16 19:19:01 +0530

    Add files via upload

---
+++ mysql/Rollout.sql
@@ -0,0 +1,32 @@
+CREATE TABLE `Kafkaconfig` (
+  `conf_id` integer,
+  `topic_name` varchar(255),
+  `database_id` integer,
+  `table_name` varchar(50),
+  `merge_on_src` varchar(255),
+  `merge_on_dest` varchar(255),
+  `date_fields` varchar(255),
+  `source_columns` varchar(255),
+  `dest_columns` varchar(255),
+  `process_id` integer
+);
+
+CREATE TABLE `databaseconfig` (
+  `database_id` integer,
+  `database_type` varchar(255),
+  `database_host` varchar(50),
+  `database_port` varchar(50),
+  `database_user` varchar(255),
+  `database_pass` varchar(255),
+  `database_name` varchar(255)
+);
+
+CREATE TABLE `processstatus` (
+  `id` integer,
+  `process_id` integer,
+  `active_status` varchar(1)
+);
+
+ALTER TABLE `Kafkaconfig` ADD FOREIGN KEY (`database_id`) REFERENCES `databaseconfig` (`database_id`);
+
+ALTER TABLE `Kafkaconfig` ADD FOREIGN KEY (`process_id`) REFERENCES `processstatus` (`id`);
